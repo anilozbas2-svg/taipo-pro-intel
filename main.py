651e158f0139ff3a732c92f234b3291689a95d07
anilozbas2-svg <anilozbas2@gmail.com>
2026-01-26 16:47:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2342,10 +2342,10 @@ text = build_alarm_message(
 )
 
 # ✅ Alarm mesajının sonuna ekle
-if tomorrow_perf_section:
+    if tomorrow_perf_section:
     text = text + tomorrow_perf_section
 
-await context.bot.send_message(
+    await context.bot.send_message(
     chat_id=int(ALARM_CHAT_ID),
     text=text,
     parse_mode=ParseMode.HTML,
