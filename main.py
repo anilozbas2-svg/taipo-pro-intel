de516dc5d72666fc5f9f050a57e469aeaec4f2d6
anilozbas2-svg <anilozbas2@gmail.com>
2026-05-20 23:47:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2429,11 +2429,11 @@ def _apply_signals_with_threshold(rows: List[Dict[str, Any]], xu100_change: floa
         quality_score = acc_quality_score(r, reg)
         r["acc_quality_score"] = quality_score
         
-        if in_topN and (0.00 <= ch <= 1.20) and quality_score >= 35:
+        if in_topN and (-0.30 <= ch <= 2.50) and quality_score >= 10:
             r["signal"] = "🧲"
             r["signal_text"] = "TOPLAMA"
             continue
-        if in_topN and (-1.20 <= ch < 0.00) and quality_score >= 30:
+        if in_topN and (-3.00 <= ch < 0.20) and quality_score >= 10:
             r["signal"] = "🪝"
             r["signal_text"] = "DİP TOPLAMA"
             continue
